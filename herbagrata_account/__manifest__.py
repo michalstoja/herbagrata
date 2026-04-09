0056e35fef6d70bbe5297656853d5934840acd1a
achulii <96129046+achulii@users.noreply.github.com>
2026-04-09 17:56:08 +0200

    [ADD] herbagrata_account

---
+++ herbagrata_account/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Herbagrata Account",
+    "category": "Accounting",
+    "license": "Other proprietary",
+    "author": "Data Dance s.r.o.",
+    "website": "https://www.datadance.eu",
+    "version": "19.0.1.0.0",
+    "depends": ["account"],
+    "data": [
+        "views/report_invoice.xml",
+    ],
+    "installable": True,
+}
